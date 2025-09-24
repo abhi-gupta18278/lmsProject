3bf8fd2bcf31485eda181469783f7feee8dc7d4b
Abhishek Gupta <vivekgupta18278@gmail.com>
2025-09-24 10:15:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://lmsbyabhi.onrender.com/",
+    origin: "https://lmsbyabhi.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
